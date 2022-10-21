74a1ec072904fd156b0bd56bb22d1b6795686084
Jader Raniere Albrez <jader.ralbrez@gmail.com>
2022-10-21 17:48:54 -0300

    feat: inserção do component shared header fix: ajuste do asset de logo

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -5,6 +5,7 @@ import { RoutingModule } from './routing.module';
 //Pages
 import { HomeComponent } from './home/home.component';
 import { DetailsComponent } from './details/details.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 
@@ -15,7 +16,8 @@ import { DetailsComponent } from './details/details.component';
   ],
   imports: [
     CommonModule,
-    RoutingModule
+    RoutingModule,
+    SharedModule
   ]
 })
 export class PagesModule { }
