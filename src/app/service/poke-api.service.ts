71234354365f8259bf72c3471802c4c339eba996
Jader Raniere Albrez <jader.ralbrez@gmail.com>
2022-10-23 18:11:06 -0300

    feat: adicionados métodos de consumo da api pokeapi

--- src/app/service/poke-api.service.ts
+++ src/app/service/poke-api.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { map, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -14,11 +14,23 @@ export class PokeAPIService {
     private http: HttpClient
   ) { }
 
-  get apiListAllPokemons(): Observable<any>{
+  get apiListAllPokemons():Observable<any>{
     return this.http.get<any>(this.url).pipe(
-      tap(res => res),
-      tap(res => {
-        console.log(res)
+      tap( res => res),
+      tap( res => {
+        res.results.map( (resPokemons: any) => {
+          this.apiGetPokemons(resPokemons.url).subscribe(
+            res => resPokemons.status = res
+          )
+        })
       })
-  )}
+    )
+  }
+  public apiGetPokemons(url: string):Observable<any>{
+    return this.http.get<any>(url).pipe(
+      map(
+        res => res
+      )
+    )
+  }
 }
