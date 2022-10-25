d6646cfb4fd9b41635a40857a7d0a0c1f37a5217
Jader Raniere Albrez <jader.ralbrez@gmail.com>
2022-10-25 17:55:29 -0300

    feat: adicionado filtro de pokemons fix: ajustes no scss de poke-list feat: configurado componente de busca e seu Output;

--- src/app/shared/poke-list/poke-list.component.ts
+++ src/app/shared/poke-list/poke-list.component.ts
@@ -8,7 +8,9 @@ import { PokeAPIService } from 'src/app/service/poke-api.service';
 })
 export class PokeListComponent implements OnInit {
 
+  private setAllPokemons: any;
   public getAllPokemons: any;
+
   constructor(
     private pokeApiService: PokeAPIService
   ) { }
@@ -16,9 +18,18 @@ export class PokeListComponent implements OnInit {
   ngOnInit(): void {
     this.pokeApiService.apiListAllPokemons.subscribe(
       res => {
-        this.getAllPokemons = res.results;        
+        this.setAllPokemons = res.results;
+        this.getAllPokemons = this.setAllPokemons;   
       }
     );
+  }
+
+  public getSearch(value: string){
+    const filter = this.setAllPokemons.filter((res: any) =>{
+      return !res.name.indexOf(value.toLowerCase());
+    });
+
+    this.getAllPokemons = filter;
   }
 
 }
