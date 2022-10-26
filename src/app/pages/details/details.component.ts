1e6978904968d53cf261ab3149fc000726bbfdf3
Jader Raniere Albrez <jader.ralbrez@gmail.com>
2022-10-26 20:10:28 -0300

    feat: início da configuração da página de details. adição dos métodos e rotas

--- src/app/pages/details/details.component.ts
+++ src/app/pages/details/details.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { forkJoin } from 'rxjs';
+
+//Services
+import { PokeAPIService } from 'src/app/service/poke-api.service';
 
 @Component({
   selector: 'app-details',
@@ -7,9 +12,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailsComponent implements OnInit {
 
-  constructor() { }
+  private urlPokemon: string = 'https://pokeapi.co/api/v2/pokemon'
+  private urlName: string = 'https://pokeapi.co/api/v2/pokemon-species'
+
+  constructor(
+    private activatedRoute : ActivatedRoute,
+    private pokeAPIService: PokeAPIService
+  ) {}
 
   ngOnInit(): void {
+    this.pokemon;
   }
 
+  get pokemon(){
+    const id = this.activatedRoute.snapshot.params['id'];
+    const pokemon = this.pokeAPIService.apiGetPokemons(`${this.urlPokemon}/${id}`)
+    const name = this.pokeAPIService.apiGetPokemons(`${this.urlName}/${id}`)
+
+    return forkJoin([pokemon, name]).subscribe(
+      res =>{
+        console.log(res);
+      }
+    )
+  };
 }
